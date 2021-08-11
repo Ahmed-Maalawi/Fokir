5c287c2a6b059c2c83e8608b063e38ab5adc1824
Ahmed-Maalawi <ahmedmaalawy001101@gmail.com>
2021-08-11 23:07:02 +0200

    add fillter

--- js/order.js
+++ js/order.js
@@ -27,32 +27,6 @@ barsBtn.addEventListener('click', () => {
     navBarUl.classList.toggle('hide');
 });
 
-
-
-
-// $(function () {
-//     var titleWidth = $('.animation').outerWidth();
-
-//     "use strict";
-
-//     (function inmate_title() {
-
-
-//         $('.animation').animate({
-    
-//             width: titleWidth+'px',
-//         }, 3000, function () {
-//             $(this).animate({
-//                 width: '-30px'
-//             }, 3000, function (){
-//                 inmate_title();
-//             });
-//         });
-//     }());
-
-// console.log(titleWidth);
-// });
-
 // ----------------- toggle active class when scroll -----------
 var sections = document.querySelectorAll('section');
 
@@ -84,7 +58,6 @@ onscroll = function () {
     } );
 }
 
-
 function removeActiveClasses () {
 
     'use strict';
@@ -95,7 +68,6 @@ function removeActiveClasses () {
     });
 
 }
-
 
 function addActiveClass(id){
 
@@ -109,6 +81,98 @@ function addActiveClass(id){
 
 // ----------- loading animation ----------------
 window.addEventListener('load', () => {
+
     const pageload = document.querySelector('.loading-page');
-        pageload.classList.add('hidden');
-})
+
+    pageload.classList.add('hidden');
+
+});
+
+// -----------------------------------------------
+
+const exitBtn = document.getElementById('exit-btn');
+var images = document.querySelectorAll('.image');
+
+images.forEach((image) => {
+
+    image.addEventListener('click', () => {
+
+        let imageSrc = image.childNodes[1].src;
+
+        document.querySelector('.display-box img').src= imageSrc;
+
+        exitBtn.parentElement.classList.remove('hidden');
+    });
+});
+
+
+exitBtn.addEventListener('click', function() {
+
+    this.parentElement.classList.add('hidden');
+});
+
+// --------------------(filltering code) -----------
+
+const filterBtns = document.querySelectorAll('#portfolio ul button');
+
+filterBtns.forEach(btn => {
+
+    btn.addEventListener('click', (event) => {
+
+        filterBtns.forEach(item => {
+
+            if(item.classList[1] == 'active-btn') {
+
+                item.classList.remove('active-btn');
+            }
+        });
+
+        event.target.classList.add('active-btn');
+
+        var sec = event.target.textContent;
+
+        if(sec == 'graphic') {
+
+            filterImg(sec);
+
+        } else if (sec == 'web design'){
+
+            filterImg('web-design');
+
+        } else if (sec == 'branding') {
+
+            filterImg(sec);
+            
+        } else {
+
+            displayAllImg();
+        }
+    });
+});
+
+function filterImg(section) {
+
+    var allImg = document.querySelectorAll('.image');
+
+    allImg.forEach(img => {
+
+        if(img.classList[1] != section) {
+            
+            img.classList.add('d-none');
+        } else {
+            
+            img.classList.remove('d-none');
+        }
+    });
+}
+
+function displayAllImg() {
+
+    var allImg = document.querySelectorAll('.image');
+
+    allImg.forEach(item => {
+
+        if(item.classList[2] == 'd-none')
+            item.classList.remove('d-none');
+    });
+}
